9efda551ea3da462d15c879a3d783ea46fd9c81a
kulnaak <kulnaak@gmail.com>
2024-05-30 11:25:47 +0800

    moved index.html

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
 
 // Define a route for the home page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server and listen on the defined port
 app.listen(PORT, () => {
-  console.log(`Server is running   on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
